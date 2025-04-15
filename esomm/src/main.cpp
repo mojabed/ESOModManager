6e2c85e88bf4acef2c572d668839eb450585b0ca
Mo <mja3u@mtmail.mtsu.edu>
2025-04-15 03:47:25 -0500

    Complete project setup

--- esomm/src/main.cpp
+++ esomm/src/main.cpp
@@ -1,10 +1,12 @@
 #include "esomm.h"
 #include <QtWidgets/QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     ESOMM mainWindow;
+    mainWindow.setWindowIcon(QIcon(":/esomm.ico"));
     mainWindow.show();
 
     return app.exec();
